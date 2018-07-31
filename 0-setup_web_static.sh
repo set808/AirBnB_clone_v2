a97ff7c8d59956e31a56f297b3256c9415786c74
Spencer Taylor <spencertaylor808@gmail.com>
2018-07-31 03:12:38 +0000

    Add index.html file creation

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,6 +7,7 @@ sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "39i $STRING" $FILE
