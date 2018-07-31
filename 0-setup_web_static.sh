627cf559f891af5d21af20c63c367b5aa67b050b
Spencer Taylor <spencertaylor808@gmail.com>
2018-07-31 02:08:20 +0000

    Create 0-setup_web_static.sh

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Sets up server for the deployment of web_static
+FILE=/etc/nginx/sites-available/default
+STRING="location /hbnb_static {\n alias /data/web_static/current; \n}\n"
+
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "39i $STRING" $FILE
+sudo service nginx restart
